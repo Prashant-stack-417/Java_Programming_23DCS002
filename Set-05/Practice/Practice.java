74d2c7f607afde0833c2885889116783704493d1
Prashant-stack-417 <23dcs002@charusat.edu.in>
2024-08-13 11:08:59 +0530

    Practice

---
+++ Set-05/Practice/Practice.java
@@ -0,0 +1,42 @@
+// public class error {
+//     public static void main(String[] args) {
+//         // int a = 3, b = 0, c;
+//         // System.out.println("before");
+//         // try {
+//         // c = a / b;
+//         // } catch (Exception e) {
+//         // System.out.println("Exception resolved ");
+//         // System.out.println(e.toString());
+//         // System.out.println(e.getMessage());
+//         // e.printStackTrace();
+//         // }
+
+//         // System.out.println("After");
+
+//         int a[] = { 0, 1, 2 };
+//         try {
+//             System.out.println(a[3]);
+//         } catch (Exception e) {
+//             System.out.println("handeld" + e);
+//         }
+
+//         String s = "Charusat";
+//         try {
+//             System.out.println(s.charAt(9));
+//         } catch (Exception e) {
+
+//             System.out.println("Handeled" + e);
+//         }
+
+//         String s1 = null;
+//         try
+//         {
+//             System.out.println(s1.length());
+//         }
+//         catch(Exception e)
+//         {
+//             System.out.println("handeled" + e);
+
+//         }
+//     }
+// }
